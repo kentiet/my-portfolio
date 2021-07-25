14db47e9805c4eaf54dc76af2694d84656f24813
Ken Tiet <ken.tietkien@gmail.com>
2021-07-24 23:16:23 -0700

    add logo picture

--- src/component/logo/logo.component.jsx
+++ src/component/logo/logo.component.jsx
@@ -3,13 +3,7 @@ import './logo.styles.scss'
 
 const Logo = () => (
     <div className="animate__animated animate__bounceInLeft logo">
-        <div className="l1">#</div>
-        <div className="l2">Ken</div>
-        <div className="l2">Tiet</div>
-        <div className="l3">#</div>
-        {/* <div className="l1">k3n</div>
-        <div className="l2">&#8800;</div>
-        <div className="l3">T.</div> */}
+        <img src="https://user-images.githubusercontent.com/16614788/126889570-19aaa09a-2b12-4f7f-b6ba-bb70bf7c7416.jpg" alt="logo"  />
     </div>
 )
 
